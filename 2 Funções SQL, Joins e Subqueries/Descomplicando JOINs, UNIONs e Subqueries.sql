742df0407f3cdb2a72f101b787bf36be9aa222d0
LeoFuna <leo.funa@gmail.com>
2021-08-25 14:50:46 -0300

    Exercicios de SELF JOIN

--- 2 Funções SQL, Joins e Subqueries/Descomplicando JOINs, UNIONs e Subqueries.sql
+++ 2 Funções SQL, Joins e Subqueries/Descomplicando JOINs, UNIONs e Subqueries.sql
@@ -18,3 +18,10 @@ WHERE district = 'California' AND first_name LIKE '%rene%';
 SELECT A.actor_id, A.first_name, FA.film_id, F.title FROM actor AS A
 INNER JOIN film_actor AS FA ON A.actor_id = FA.actor_id
 INNER JOIN film AS F ON FA.film_id = F.film_id;
+
+----------------
+SELECT f1.film_id, f1.replacement_cost, f2.film_id, f2.replacement_cost FROM film AS f1, film AS f2
+WHERE f1.replacement_cost = f2.replacement_cost;
+
+SELECT f1.title, f1.rental_duration, f2.title, f2.rental_duration FROM film AS f1, film AS f2
+WHERE f1.length = f2.length AND (f1.rental_duration BETWEEN 2 AND 4) AND (f2.rental_duration BETWEEN 2 AND 4);
