6c0b2ce5c20694e58f604183f58c6c50256caaff
LeoFuna <leo.funa@gmail.com>
2021-08-27 08:34:39 -0300

    Exercicios propostos

--- 2 Funções SQL, Joins e Subqueries/3 Stored Routines & Stored Functions.sql
+++ 2 Funções SQL, Joins e Subqueries/3 Stored Routines & Stored Functions.sql
@@ -126,3 +126,36 @@ BEGIN
 	INSERT INTO log_operacoes(tipo_operacao, data_ocorrido) VALUES ('EXCLUSAO', DATE(NOW()));
 END $$
 DELIMITER ;
+
+----------------------
+USE BeeMovies;
+DELIMITER $$
+
+CREATE TRIGGER trigger_movies_insert
+	BEFORE INSERT ON movies
+    FOR EACH ROW
+BEGIN
+	SET NEW.release_year = YEAR(NOW());
+END $$
+
+CREATE TRIGGER trigger_movies_logs_insert
+	AFTER INSERT ON movies
+    FOR EACH ROW
+BEGIN
+	INSERT INTO movies_logs(movie_id, executed_action, log_date) VALUES(NEW.movie_id, 'INSERT', DATE(NOW()));
+END $$
+
+DELIMITER ;
+
+USE BeeMovies;
+DELIMITER $$
+
+CREATE TRIGGER trigger_movies_update
+	BEFORE UPDATE ON movies
+    FOR EACH ROW
+BEGIN
+	SET NEW.ticket_price_estimation = IF(NEW.ticket_price > OLD.ticket_price, 'Increasing', 'Decreasing');
+    INSERT INTO movies_logs(movie_id, executed_action, log_date) VALUES (NEW.movie_id, 'UPDATE', DATE(NOW()));
+END $$
+
+DELIMITER ;
