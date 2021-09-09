a7f5d2e19681850fc08381b8883812e7835316c3
LeoFuna <leo.funa@gmail.com>
2021-09-09 11:41:52 -0300

    Exercicio 7  mongoDB count

--- 4 Introdução ao MongoDb/1 MongoDB Introdução.js
+++ 4 Introdução ao MongoDb/1 MongoDB Introdução.js
@@ -11,8 +11,5 @@ db.bios.find({ 'name.first': 'John' }).pretty()
 db.bios.find().limit(3).pretty()
 // Exercício 6 : Retorne 2 documentos da coleção bios pulando os 5 primeiros documentos.
 db.bios.find().skip(5).limit(2)
-// Utilizando o mongoimport , importe o arquivo books.json para a sua instância local do MongoDB e utilize a coleção books para construir as seguintes consultas:
-// Exercício 7 : Retorne a quantidade de documentos da coleção books .
-// Exercício 8 : Conte quantos livros existem com o status = "PUBLISH" .
-// Exercício 9 : Exiba os atributos title , isbn e pageCount dos 3 primeiros livros. NÃO retorne o atributo _id .
-// Exercício 10: Pule 5 documentos e exiba os atributos _id , title , authors e status dos livros com o status = "MEAP" , limitando-se a 10 documentos.
+// Exercício 7 : Retorne a quantidade de documentos da coleção bios .
+db.bios.count()
