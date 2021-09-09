a5eabcf722711aaab831df22dd79f7a2520933ff
LeoFuna <leo.funa@gmail.com>
2021-09-09 15:19:04 -0300

    Exercicios Gerais do Bloco: ex 2-5

--- 4 Introdução ao MongoDb/2 Filter Operators.js
+++ 4 Introdução ao MongoDb/2 Filter Operators.js
@@ -42,9 +42,10 @@ db.restaurants.deleteMany({ cuisine: "American" })
 // Exercício 1: Inspecione um documento para que você se familiarize com a estrutura. Entenda os atributos e os níveis existentes.
 db.superheroes.find().limit(1)
 // Exercício 2: Selecione todos os super-heróis com menos de 1.80m de altura. Lembre-se de que essa informação está em centímetros.
-
+db.superheroes.find({ 'aspects.height': { $lt: 180 } })
 // Exercício 3: Retorne o total de super-heróis menores que 1.80m.
-
+db.superheroes.count({ 'aspects.height': { $lt: 180 } })
 // Exercício 4: Retorne o total de super-heróis com até 1.80m.
-
+db.superheroes.count({ 'aspects.height': { $lte: 180 } })
 // Exercício 5: Selecione um super-herói com 2.00m ou mais de altura.
+db.superheroes.findOne({ 'aspects.height': { $gte: 200 } })
