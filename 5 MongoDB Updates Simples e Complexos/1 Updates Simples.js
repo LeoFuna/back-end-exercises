83f39a0611f0a7296f542555beb727837e4a265d
LeoFuna <leo.funa@gmail.com>
2021-09-13 10:45:51 -0300

    Exercicios 5 de updates no mongoDB

--- 5 MongoDB Updates Simples e Complexos/1 Updates Simples.js
+++ 5 MongoDB Updates Simples e Complexos/1 Updates Simples.js
@@ -7,7 +7,7 @@ db.movies.updateOne({ title: "Home Alone" }, { $set: { budget: 15, imdbRating: 5
 // Exercício 4 : Aumente em 2 o imdbRating do filme Batman .
 db.movies.updateOne({ title: "Batman" }, { $inc: { imdbRating: 2 } });
 // Exercício 5 : Aumente em 5 o budget do filme Home Alone .
-
+db.movies.updateOne({ title: "Home Alone" }, { $inc: { budget: 5 } });
 // Exercício 6 : Multiplique por 4 o imdbRating do filme Batman .
 // Exercício 7 : Renomeie o campo budget para estimatedBudget do filme Batman .
 // Exercício 8 : Utilize o operador $min para alterar o budget para 5 do filme Home Alone .
