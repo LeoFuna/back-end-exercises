0b62fa0aa1aaa15a26aed1e88107aa609fa07654
LeoFuna <leo.funa@gmail.com>
2021-09-14 10:57:13 -0300

    Exercicio 11 acertado

--- 5 MongoDB Updates Simples e Complexos/2 Updates Complexos - Arrays P1.js
+++ 5 MongoDB Updates Simples e Complexos/2 Updates Complexos - Arrays P1.js
@@ -40,11 +40,11 @@ db.movies.updateOne({ title: "Batman" }, { $set: { cast: [{
 }] } });
 // Exercício 11: Produza três querys para o filme Batman :
 // Adicione o campo actor , que deve ser um array com o valor Christian Bale , ao array de cast em que o campo character seja igual a Batman ;
-db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": "Christian Bale" } }, 
+db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": ["Christian Bale"] } }, 
 { arrayFilters: [{ "element.character": "Batman" }] });
 // Adicione o campo actor , que deve ser um array com o valor Michael Caine , ao array de cast em que o campo character seja igual a Alfred ;
-db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": "Michael Caine" } }, 
+db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": ["Michael Caine"] } }, 
 { arrayFilters: [{ "element.character": "Alfred" }] });
 // Adicione o campo actor , que deve ser um array com o valor Heath Ledger , ao array de cast em que o campo character seja igual a Coringa .
-db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": "Heath Ledger" } }, 
+db.movies.updateOne({ title: "Batman" }, { $set: { "cast.$[element].actor": ["Heath Ledger"] } }, 
 { arrayFilters: [{ "element.character": "Coringa" }] });
