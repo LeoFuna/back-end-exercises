c11884bc251507c30af9e0c7f8bf6cfff32c90d9
LeoFuna <leo.funa@gmail.com>
2021-09-14 10:35:35 -0300

    Exercicio 6

--- 5 MongoDB Updates Simples e Complexos/2 Updates Complexos - Arrays P1.js
+++ 5 MongoDB Updates Simples e Complexos/2 Updates Complexos - Arrays P1.js
@@ -9,5 +9,5 @@ db.movies.updateOne({ title: "Batman" }, { $pop: { category: -1 } });
 // Exercício 5: Remova o último elemento do array category do filme Batman .
 db.movies.updateOne({ title: "Batman" }, { $pop: { category: 1 } });
 // Exercício 6: Adicione o elemento "action" ao array category do filme Batman , garantindo que esse valor não se duplique.
-
+db.movies.updateOne({ title: "Batman" }, { $addToSet: { category: "action" } });
 // Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
