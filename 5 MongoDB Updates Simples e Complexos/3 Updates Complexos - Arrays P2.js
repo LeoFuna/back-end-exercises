70a35717a8bce7456cd91ebb6bc6ab8c9cfe52a0
LeoFuna <leo.funa@gmail.com>
2021-09-15 11:21:49 -0300

    Exercicio 10-11

--- 5 MongoDB Updates Simples e Complexos/3 Updates Complexos - Arrays P2.js
+++ 5 MongoDB Updates Simples e Complexos/3 Updates Complexos - Arrays P2.js
@@ -17,9 +17,9 @@ db.movies.find({ ratings: { $elemMatch: { $gt: 64, $lt: 105, $mod: [9, 0] } } },
 // 9 Retorne os filmes da categoria adventure e com ratings maior do que 103 , exibindo apenas os campos title , ratings e category .
 db.movies.find({ category: { $all: ["adventure"] }, ratings: { $elemMatch: { $gt: 103 } } }, { title: 1, ratings: 1, category: 1, _id: 0 });
 // 10 Retorne somente o título de todos os filmes com dois elementos no array category .
-
+db.movies.find({ category: { $size: 2 } }, { title: 1, _id: 0 });
 // 11 Retorne somente o título de todos os filmes com quatro elementos no array ratings .
-
+db.movies.find({ ratings: { $size: 4 } }, { title: 1, _id: 0 });
 // 12 Busque os filmes em que o módulo 5 do campo budget seja 0 e que o array category tenha tamanho 2 .
 // 13 Retorne os filmes da categoria "sci-fi" ou que possua o ratings maior do que 199 , exibindo apenas os campos title , ratings e category .
 // 14 Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family" , mas que não tenha o imdbRating menor que 7.
