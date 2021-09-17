75d4ed0ae6a4ad6c9b1eef414d5be791d4920f60
LeoFuna <leo.funa@gmail.com>
2021-09-17 11:28:16 -0300

    Exercicios do dia EX 2

--- 6 MongoDB: Aggregation Framework/Parte 1.js
+++ 6 MongoDB: Aggregation Framework/Parte 1.js
@@ -51,7 +51,10 @@ db.clientes.aggregate([{
     sexo: "MASCULINO"
   } }])
 // Exercício 2: Utilizando o estágio $match , escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005 .
-
+db.clientes.aggregate([{ 
+  $match: {
+    sexo: "FEMININO", dataNascimento: { $gt: ISODate("1995-01-01T00:00:00.0Z"), $lt: ISODate("2005-01-01T00:00:00.0Z") } }
+  }]);
 // Exercício 3: Utilizando o estágio $match , escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005 , limitando a quantidade de documentos retornados em 5 .
 
 // Exercício 4: Conte quantos clientes do estado SC existem na coleção. Retorne um documento em que o campo _id contenha a UF e outro campo com o total.
