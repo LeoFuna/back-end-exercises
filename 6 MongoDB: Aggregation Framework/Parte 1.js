145fff8212660437d3ecfff378e4ccf0c9c00697
LeoFuna <leo.funa@gmail.com>
2021-09-17 10:23:32 -0300

    Exercicios Fixaçao  e

--- 6 MongoDB: Aggregation Framework/Parte 1.js
+++ 6 MongoDB: Aggregation Framework/Parte 1.js
@@ -1,4 +1,4 @@
-// PARA FIXAR
+// PARA FIXAR $match e $limit
 //1 Selecione todas as transações feitas pelo cliente chamado "Dave America".
 db.transactions.aggregate([{ $match: { from: "Dave America" } }]);
 //2 Selecione todas as transações com o valor entre 700 e 6000, ou que sejam recebidas pela cliente "Lisa Simpson".
@@ -6,5 +6,9 @@ db.transactions.aggregate([{ $match: { $or: [{ value: { $gt: 700, $lt: 6000 } },
 //3 Selecione três transações com o valor acima de 1000.
 db.transactions.aggregate([{ $match: { value: { $gt: 1000 } }}, { $limit: 3 }]);
 
-
-
+// PARA FIXAR $project e $group
+//1 Selecione todos os bancos, ou seja, valores do campo bank ;
+db.transactions.aggregate([{ $project: { _id: 0, bank: 1 } }]);
+//2 Selecione o valor total das transações em cada banco e quantas são;
+//3 Selecione o valor total de transações por banco;
+//4 Selecione os bancos que têm o valor total de transações maior que 1000.
