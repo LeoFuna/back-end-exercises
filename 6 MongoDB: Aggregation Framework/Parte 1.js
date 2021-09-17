7368df45606250b48e09d3ff909ebd52a5fd05f2
LeoFuna <leo.funa@gmail.com>
2021-09-17 11:38:07 -0300

    Exercicios do dia EX 5

--- 6 MongoDB: Aggregation Framework/Parte 1.js
+++ 6 MongoDB: Aggregation Framework/Parte 1.js
@@ -72,5 +72,14 @@ db.clientes.aggregate([
   { $project: { total: 1 } }
 ]);
 // Exercício 5: Agrupe os clientes por sexo . Retorne o total de clientes de cada sexo no campo total .
+db.clientes.aggregate([ 
+  { $group: {
+    _id: "$sexo",
+    total: {
+      $sum: 1
+    }
+  }}, 
+  { $project: { total: 1 } }
+]);
 
 // Exercício 6: Agrupe os clientes por sexo e uf . Retorne o total de clientes de cada sexo no campo total .
