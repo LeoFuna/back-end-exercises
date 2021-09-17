2f05cc292d425c6dd458bbf0fa8cb22dad56da23
LeoFuna <leo.funa@gmail.com>
2021-09-17 10:47:36 -0300

    Exercicios Fixaçao  e  finalizados

--- 6 MongoDB: Aggregation Framework/Parte 1.js
+++ 6 MongoDB: Aggregation Framework/Parte 1.js
@@ -25,6 +25,21 @@ db.clients.aggregate([{
     foreignField: 'from',
     as: 'transacoesDoCliente'
   } }]);
-  
 //2 Selecione quatro clientes com as suas respectivas transações recebidas;
+db.clients.aggregate([{
+  $lookup: {
+    from: 'transactions',
+    localField: 'name',
+    foreignField: 'to',
+    as: 'transacoesDoCliente'
+  } }]);
 //3 Selecione todos os cliente do estado da "Florida" e suas respectivas transações recebidas.
+db.clients.aggregate([
+  { $match: { State: "Florida" } },
+  { $lookup: {
+    from: 'transactions',
+    localField: 'name',
+    foreignField: 'to',
+    as: 'transacoesDoCliente'
+    } 
+  }]);
