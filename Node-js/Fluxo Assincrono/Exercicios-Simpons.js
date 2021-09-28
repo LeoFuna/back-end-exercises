0ff0a949a9c1d9a51c272e3cc23a1436c4b2eb37
LeoFuna <leo.funa@gmail.com>
2021-09-28 10:42:19 -0300

    Exercicio 1 simpsons

--- Node-js/Fluxo Assincrono/Exercicios-Simpons.js
+++ Node-js/Fluxo Assincrono/Exercicios-Simpons.js
@@ -1,7 +1,16 @@
 // Realize o download deste arquivo e salve-o como simpsons.json . Utilize o arquivo baixado para realizar os requisitos abaixo.
 // Você pode utilizar then e catch , async/await ou uma mistura dos dois para escrever seu código. Procure não utilizar callbacks.
 // 1 Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome . Por exemplo: 1 - Homer Simpson .
-const fs = require('fs');
+const fs = require('fs').promises;
+
+function imprimePersonagens() {
+  fs.readFile(__dirname + '/simpsons.json', 'utf-8') // __dirname para reconhecer o caminho do diretório, senao ele nao encontra
+    .then((response) => JSON.parse(response))
+    .then((result) => result.map((personagem) => console.log(`${ personagem.id } - ${ personagem.name }`)))
+    .catch((error) => console.log(error.message))
+}
+
+imprimePersonagens();
 
 // 2 Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
 // 3 Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
