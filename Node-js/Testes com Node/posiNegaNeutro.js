7f8f9d46cac365a42407f89c2461fad8d44fad84
LeoFuna <leo.funa@gmail.com>
2021-09-30 11:05:58 -0300

    testes e funçao posiNegaNeutro feito

--- Node-js/Testes com Node/posiNegaNeutro.js
+++ Node-js/Testes com Node/posiNegaNeutro.js
@@ -1,14 +1,17 @@
 
 function posiNegaNeutro(num) {
   switch (true) {
-    case num > 0:
+    case num > 0 && typeof num === 'number':
       return "positivo";
       break;
-    case num < 0:
+    case num < 0 && typeof num === 'number':
       return "negativo";
       break;
-    default:
+    case num === 0:
       return "neutro";
+      break;
+    default:
+      return null;
   }
 }
 
--- Node-js/Testes com Node/tests/posiNegaNeutro.test.js
+++ Node-js/Testes com Node/tests/posiNegaNeutro.test.js
@@ -4,21 +4,30 @@ const { posiNegaNeutro } = require('../posiNegaNeutro');
 describe("Verifica o dado informado quando", () => {
   describe("ele é um número", () => {
     describe("ele é maior do que zero", () => {
+      const resultado = posiNegaNeutro(4);
       it("deve ser positivo", () => {
-        const resultado = posiNegaNeutro(4);
         expect(resultado).to.be.equal("positivo");
+      })
+      it("deve ser uma string", () => {
+        expect(resultado).to.be.a("string");
       })
     })
     describe("ele é igual a zero", () => {
+      const resultado = posiNegaNeutro(0);
       it("deve ser neutro", () => {
-        const resultado = posiNegaNeutro(0);
         expect(resultado).to.be.equal("neutro");
+      });
+      it("deve ser uma string", () => {
+        expect(resultado).to.be.a("string");
       })
     })
     describe("ele é menor do que zero", () => {
+      const resultado = posiNegaNeutro(-3);
       it("deve ser negativo", () => {
-        const resultado = posiNegaNeutro(-3);
         expect(resultado).to.be.equal("negativo");
+      });
+      it("deve ser uma string", () => {
+        expect(resultado).to.be.a("string");
       })
     })
   })
