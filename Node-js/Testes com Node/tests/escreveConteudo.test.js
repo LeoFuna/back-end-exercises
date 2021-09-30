1ae3e86092f490686f828e62ff58ae52f40d1b27
LeoFuna <leo.funa@gmail.com>
2021-09-30 11:20:14 -0300

    testes criados para funçao escreveConteudo

---
+++ Node-js/Testes com Node/tests/escreveConteudo.test.js
@@ -0,0 +1,35 @@
+describe('Função que escreve conteúdo quando', () => {
+  describe('recebe os dois ou mais parâmetros', () => {
+    describe('sendo os 2 primeiros strings', () => {
+      it('retorna um ok', () => {
+
+      })
+      it('cria um arquivo com o conteúdo proposto', () => {
+
+      })
+    })
+    describe('sendo o primeiro ou segundo deles diiferente de string', () => {
+      it('retorna null', () => {
+
+      })
+    })
+  })
+  describe('recebe 1 parâmetro', () => {
+    describe('diferente de string', () => {
+
+    })
+    describe('sendo uma string', () => {
+      it('retorna um ok', () => {
+
+      })
+      it('cria um arquivo com conteúdo vazio', () => {
+
+      })
+    })
+  })
+  describe('recebe nenhum parâmetro', () => {
+    it('retorna null', () => {
+
+    })
+  })
+})
