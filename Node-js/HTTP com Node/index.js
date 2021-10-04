4387f906a151573fddfe4fdfd1c9f9965fcf4ac8
LeoFuna <leo.funa@gmail.com>
2021-10-04 15:40:29 -0300

    Exercicio 3

--- Node-js/HTTP com Node/index.js
+++ Node-js/HTTP com Node/index.js
@@ -17,3 +17,9 @@ app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ "message": `Hello, ${ name }!` })
 })
+
+app.post('/greetins', (req, res) => {
+  const { age, name } = req.body;
+  if (age <= 17 ) return res.status(401).json({ "message": "Unauthorized" });
+  res.status(200).json({ "message": `Hello, ${ name }` });
+})
