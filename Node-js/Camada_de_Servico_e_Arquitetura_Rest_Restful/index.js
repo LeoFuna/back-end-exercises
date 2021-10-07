2a972003d452a019f40520893afac4e9463624ea
LeoFuna <leo.funa@gmail.com>
2021-10-07 13:59:25 -0300

    modelo de listar todos os livros implementado

--- Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/index.js
+++ Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Author = require('./models/authors');
+const Book = require('./models/book');
 
 const app = express();
 
@@ -10,7 +11,12 @@ app.listen(PORT, () => {
   console.log('Ouvindo a porta 3000')
 });
 
-app.get('/authors', async (req, res) => {
+app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
   res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Book.getAll();
+  res.status(200).json(books);
 });
---
+++ Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/models/book.js
@@ -0,0 +1,12 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [books] = await connection.execute(
+    'SELECT * FROM books;'
+  );
+  return books;
+}
+
+module.exports = {
+  getAll
+};
