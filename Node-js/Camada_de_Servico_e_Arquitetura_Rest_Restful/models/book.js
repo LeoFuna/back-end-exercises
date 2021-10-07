59b0881b99d9ad7eb17cd6697d5f829522dccd89
LeoFuna <leo.funa@gmail.com>
2021-10-07 14:05:20 -0300

    testes no exercicio

--- Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/models/book.js
+++ Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/models/book.js
@@ -1,7 +1,7 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [books] = await connection.execute(
+  const [books] = await connection.execute( // desestruturação para pegar somente o primeiro item do array, pois é o que de fato interessa da resposta
     'SELECT * FROM books;'
   );
   return books;
