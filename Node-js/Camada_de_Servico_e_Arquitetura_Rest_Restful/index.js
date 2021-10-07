8217c8d56171f3965373c30656e85075c7ef57e8
LeoFuna <leo.funa@gmail.com>
2021-10-07 13:48:58 -0300

    configuraçoes iniciais do modelo

--- Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/index.js
+++ Node-js/Camada_de_Servico_e_Arquitetura_Rest_Restful/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const Author = require('./models/authors');
+
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('Ouvindo a porta 3000')
+});
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.getAll();
+  res.status(200).json(authors);
 });
