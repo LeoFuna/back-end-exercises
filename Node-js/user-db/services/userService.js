67bd560bc905c02517fbcc1af5f8563de75251b7
LeoFuna <leo.funa@gmail.com>
2021-10-08 15:33:29 -0300

    Resolvendo erro na validaçao do password

--- Node-js/user-db/services/userService.js
+++ Node-js/user-db/services/userService.js
@@ -8,7 +8,7 @@ const getAll = async () => {
 const create = async (firstName, lastName, email, password) => {
   const error = 404;
   if(!firstName || !lastName || !email || !password) return { error, message: 'Todos os campos são obrigatórios!' };
-  if(password.length < 6) return { error, message: "A senha deve ter o mínimo de 6 caracteres!" };
+  if(String(password).length < 6) return { error, message: "A senha deve ter o mínimo de 6 caracteres!" };
   if(typeof password !== 'number') return { error, message: 'A senha deve ser somente de números!' };
   const response = await Users.create(firstName, lastName, email, password);
   return response;
